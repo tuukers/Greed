fef3c004942352ea01e8bd8b1e064d176c0c708a
tuukers <ethangraham321abc@gmail.com>
2022-06-06 10:55:03 -0600

    Program Setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Unit04.Game.Casting;
+using Unit04.Game.Directing;
+using Unit04.Game.Services;
+
+
+namespace Unit04
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
